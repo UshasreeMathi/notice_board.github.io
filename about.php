19bab4b09e6be7a68eb4b0333817a7bd9caf4ad6
UshasreeMathi <69321356+UshasreeMathi@users.noreply.github.com>
2020-08-30 22:31:34 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,2 @@
+<h2><font color=blue>About</font></h2>
+<h3>Here we can Advertise anything and send the important notifications to the user </h3>
---
+++ contact.php
@@ -0,0 +1,48 @@
+<h2><font color=blue>Quick Contact</font></h2>
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	<div class="row">
+		<div class="col-sm-4"> YOur Email</div>
+		<div class="col-sm-5">
+		<input type="email" name="e" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4"> YOur Mobile</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4"> YOur Message</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Send Query" name="save" class="btn btn-success"/>
+		
+		</div>
+	</div>
+		
+		</div>
+		<div class="col-md-4">
+		<h2>Contact us</h2>
+		
+		Name: Ushasree Mathi<br/>
+		Email:Ushasreemathi555@gmail.com<br/>
+		</div>
+	</div>
+	
+	
+</form>	
